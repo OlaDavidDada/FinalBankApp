770a510403b462a794071d9e5e3604a45c244da8
Ola <oladadadavid@gmail.com>
2023-03-27 01:43:40 +0100

    App finally works

---
+++ functions.py
@@ -0,0 +1,55 @@
+#parent class
+import random 
+
+class User():
+    def __init__(self, name):
+        self.name = name
+        
+    
+    def showDetails(self):
+        accountNumber = random.randint(00000000, 99999999)
+        # a = accountNumber
+        x = 'Personal details'
+        y = 'Name: ' + self.name
+        z = 'Account balance is now £' + str(self.balance) 
+        a = 'Account Number is ' + str(accountNumber)
+        result = [x,y,z,a]
+        return result
+        
+
+#child class
+class Bank(User):
+    def __init__(self, name):
+        super().__init__(name)
+        self.balance = 0
+    
+    def deposit(self, amount):
+        self.amount = int(amount)
+        self.balance = self.amount + self.balance
+        return 'Account balance is now £' + str(self.balance)
+    
+    def withdraw(self, amount):
+        self.amount = int(amount)
+        if self.amount > self.balance:
+            return 'Insufficient funds £' + str(self.balance)
+        else:
+            self.balance = self.balance - self.amount
+            return 'Account balance is now £' + str(self.balance)
+    
+    # def transfer(self, amount):
+    #     self.amount = int(amount)
+    #     if self.amount > self.balance:
+    #         return 'Insufficient funds £' + str(self.balance)
+    #     else:
+    #         self.balance = self.balance - self.amount
+    #         return 'Account balance is now £' + str(self.amount)
+
+    def viewBalance(self):
+        return str(self.balance)
+
+# Ola = Bank('Ola')
+# print(Ola.showDetails())
+# print(Ola.deposit(1000))
+# print(Ola.showDetails()) 
+# print(Ola.withdraw(200))   
+# print(Ola.showDetails())
---
+++ main.py
@@ -0,0 +1,96 @@
+# Banking Website
+from flask import Flask, render_template, request, session, url_for, redirect
+
+from flask.globals import request
+
+from functions import Bank
+
+app = Flask(__name__)
+users = {}
+print(users)
+app.secret_key = 'hkjkjhft7yqr7526%$^"£"'
+
+
+@app.route('/')
+def home():
+  if 'user' in session:
+    money = user.viewBalance()
+    return render_template('home.html', money=money)
+  else:
+    return render_template('home.html')
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+  global user
+  if request.method == 'POST':
+    username = request.form['username']
+    password = request.form['password']
+
+    if username in users and users[username] == password:
+      session['user'] = username
+
+      user = Bank(session['user'])
+
+      print(user.showDetails())
+
+      print('Login Successful')
+      return render_template('home.html')
+    else:
+      print('Email dosent exist or wrong password')
+      return render_template('login.html')
+      
+
+  return render_template('login.html')
+
+@app.route('/register', methods= ['GET', 'POST'])
+def register():
+  if request.method == 'POST':
+    username = request.form['username']
+    if username in users:
+      print('User already exists')
+      return render_template('register.html')
+    else:
+      password = request.form['password']
+      users[username] = password
+      print('user created')
+      print(users)
+      return render_template('home.html')
+  else:
+    return render_template('register.html')
+
+
+@app.route('/deposit', methods = ['GET', 'POST'])
+def deposit():
+  if request.method == 'POST':
+    amount = request.form['deposit']
+    user.deposit(amount)
+    print(user.showDetails())
+    return render_template('deposit.html')
+  else:
+    return render_template('deposit.html')
+
+@app.route('/withdraw', methods = ['GET', 'POST'])
+def withdraw():
+  if request.method == 'POST':
+    amount = request.form['withdraw']
+    user.withdraw(amount)
+    print(user.showDetails())
+    return render_template('withdraw.html')
+  else:
+    return render_template('withdraw.html')
+
+@app.route('/logout')
+def logout():
+  session.pop('user', None)
+  return redirect(url_for('home'))
+  
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
+
+
+
+
+
